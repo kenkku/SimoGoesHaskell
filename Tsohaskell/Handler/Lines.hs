50afef1916bdcec58c3e4fc8c5408030f2887a82
Tsarpf <tsarpf@gmail.com>
2013-06-11 03:53:06 +0300

    website.net/lines now shows nick and message from all lines in the database

--- Tsohaskell/Handler/Lines.hs
+++ Tsohaskell/Handler/Lines.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE ScopedTypeVariables, QuasiQuotes #-}
+
 module Handler.Lines where
 
 import Import
+import Network.HTTP.Types (status200)
 
-getLinesR :: Handler Html
-getLinesR = error "Not yet implemented: getLineR"
+
+
+
+getLinesR :: Handler RepHtml
+getLinesR = do 
+    values :: [Entity Line] <- (runDB $ selectList [] []) 
+    defaultLayout [whamlet|
+<ul>
+    $forall line <- values
+        <li>#{lineNick (entityVal line)} #{lineMessage (entityVal line)}
+|] 
+
+--   liftIO $ print (values :: [Entity Line])
