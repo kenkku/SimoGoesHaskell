1af8b85e55858a95c7573e3ce3dd8a152968053c
Tsarpf <tsarpf@gmail.com>
2013-06-11 03:57:14 +0300

    Moved html stuff from lines handler to a template

--- Tsohaskell/Handler/Lines.hs
+++ Tsohaskell/Handler/Lines.hs
@@ -11,10 +11,5 @@ import Network.HTTP.Types (status200)
 getLinesR :: Handler RepHtml
 getLinesR = do 
     values :: [Entity Line] <- (runDB $ selectList [] []) 
-    defaultLayout [whamlet|
-<ul>
-    $forall line <- values
-        <li>#{lineNick (entityVal line)} #{lineMessage (entityVal line)}
-|] 
+    defaultLayout $(widgetFile "lines")
 
---   liftIO $ print (values :: [Entity Line])
