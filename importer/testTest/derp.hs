fbe94be73c537a9ded8828f0aa1ba0c40028f93c
Tsarpf <tsarpf@gmail.com>
2013-06-19 07:54:05 +0200

    Channel messages can now be parsed to a list of tuples, where the first part is the day of the message and the second part is a list of the days messages

--- importer/testTest/derp.hs
+++
@@ -1,4 +0,0 @@
-main = do 
-    putStrLn "Penis"
-    name <- getLine
-    putStrLn ("Hello, " ++ name ++ ", how are you?")
--- importer/testTest/test.hs
+++ importer/testTest/test.hs
@@ -14,30 +14,32 @@ main = do
         
         let linesInFile = lines fileContents
 
-        let days = parseDays $ filter (isPrefixOf "---") $ linesInFile
+        let days = parseDays $ filter (isPrefixOf "--- ") $ linesInFile
+        --let days = filter (isPrefixOf "--- ") $ linesInFile
         --print days
 
 
-        let messages = tail $ splitWhen (isPrefixOf "---") $ linesInFile 
+        let messages = tail $ splitWhen (isPrefixOf "--- ") $ linesInFile 
 
 
         --print $ head messages
 
         let juttuja = zip days messages
 
-        --print $ head juttuja
+        print $ juttuja
 
         return ()
         
-parseDays days = map parseDay days
-    where parseDay str = 
+parseDays days = map parseDay days 
 
+parseDay day
+    | isPrefixOf "--- Day" day = parseDayChanged day
+    | isPrefixOf "--- Log opened" day = parseLogOpened day
 
+parseDayChanged dayChanged =
+    let splitDayChanged = splitPlaces [11,50] $ drop 16 dayChanged
+        dayChangedUTC = (head splitDayChanged) ++ "00:00:00 UTC " ++ (last splitDayChanged)
+    in parseTime defaultTimeLocale "%c" dayChangedUTC :: Maybe UTCTime
 
-
-
-
-
-
-        
+parseLogOpened logOpened = parseTime defaultTimeLocale "%c" (drop 15 logOpened) :: Maybe UTCTime
 
