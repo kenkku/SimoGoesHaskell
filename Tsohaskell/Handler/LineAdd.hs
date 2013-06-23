8c64d3319a12ac14adc8e481dc569f20bff88a80
Tero Keinänen <kenkku@kenkku.net>
2013-06-23 18:45:44 +0300

    make it pretty

--- Tsohaskell/Handler/LineAdd.hs
+++ Tsohaskell/Handler/LineAdd.hs
@@ -2,13 +2,9 @@
 module Handler.LineAdd where
 
 import Import
-import Data.Aeson.TH (deriveFromJSON)
 import Data.Aeson.Types
-import Data.Char (toLower)
 import Data.Text (unpack, pack)
 import Text.Regex.TDFA
-
-$(deriveFromJSON (drop 4 . map toLower) ''Line)
 
 postLineAddR :: Handler RepPlain
 postLineAddR = do 
