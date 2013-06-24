81bd839ec719156c55c1e9a2fd7e6a0e37942305
Tero Keinänen <kenkku@kenkku.net>
2013-06-24 05:45:10 +0300

    view for links

---
+++ Tsohaskell/Handler/Links.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Handler.Links where
+
+import Import
+import Database.Esqueleto
+import Data.Int (Int64)
+import qualified Yesod.Links as L
+
+getLinksR :: Handler Html
+getLinksR = defaultLayout $ linksWidget 10
+
+linksWidget :: Int64 -> Widget
+linksWidget num = do
+    linkEntities :: [(Entity Link, Entity Line)] <- handlerToWidget $ runDB $ 
+        select $
+        from $ \(link, line) -> do
+            where_ (link ^. LinkLine Database.Esqueleto.==. line ^. LineId)
+            orderBy [desc (line ^. LineTime)]
+            limit num
+            return (link, line)
+    let links = map (\(link, line) -> (entityVal link, entityVal line)) linkEntities
+    $(widgetFile "links")
+        where
+            mkLink t = L.link' $ L.Link (L.External t) t t
